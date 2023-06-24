df98d26a3ad3b577c99e7ae75bf9f200f8310222
silencelamb <daixu0113@gmail.com>
2023-06-24 11:57:10 +0000

    solve host=2 device_per_host=4 bug; support any host and device_num config

--- benchmark/alpa/run.sh
+++ benchmark/alpa/run.sh
@@ -5,6 +5,8 @@ python benchmark.py --suite  gpt.grid_search_auto --num-hosts 8 --num-devices-pe
 # config parallel, anylytical model gpu
 python benchmark.py --suite  gpt.config_test --num-hosts 1 --num-devices-per-host 2 --no-separate-process --only-mapping --use-analytical-perf-model  --rst_folder tmp_a100_gpu_analytical
 
+python benchmark.py --suite  gpt.config_test --num-hosts 1 --num-devices-per-host 8 --no-separate-process --only-mapping --use-analytical-perf-model  --rst_folder tmp_a100_gpu_analytical
+
 
 # config parallel, anylytical model  wsc, 1x8
 python benchmark.py --suite  gpt.config_test --num-hosts 1 --num-devices-per-host 8 --no-separate-process --only-mapping --use-analytical-perf-model --hardware wsc --rst_folder tmp_wsc_perf_15GB_fp16
@@ -29,3 +31,5 @@ python benchmark.py --suite  gpt.config_test --num-hosts 1 --num-devices-per-hos
 
 # config parallel, anylytical model WSC
 python benchmark.py --suite  gpt.wsc_config_test --num-hosts 1 --num-devices-per-host 2 --no-separate-process --only-mapping --use-analytical-perf-model --hardware wsc  --rst_folder tmp_wsc_perf_15GB_fp16
+
+python benchmark.py --suite  gpt.wsc_config_test --num-hosts 2 --num-devices-per-host 4 --no-separate-process --only-mapping --use-analytical-perf-model --hardware wsc  --rst_folder tmp_wsc_perf_15GB_fp16
