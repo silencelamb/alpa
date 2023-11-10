35cd564d84cf1838e6714353c6b83249396c06cf
Conquer2020 <18315843193@163.com>
2023-11-10 05:23:24 +0000

    Update final batch run script for GPT, BERT, WResNet with offload and without offload.

--- benchmark/alpa/batch_run_perf.sh
+++ benchmark/alpa/batch_run_perf.sh
@@ -11,3 +11,18 @@ python benchmark.py --suite bert.wsc_perf --offload --num-hosts 6 --num-devices-
 python benchmark.py --suite wresnet.wsc_perf --num-hosts 6 --num-devices-per-host 4 --hardware wsgpu --rst_folder tmp_wsgpu
 # open offload for wresnet
 python benchmark.py --suite wresnet.wsc_perf --offload --num-hosts 6 --num-devices-per-host 4 --hardware wsgpu --rst_folder tmp_wsgpu 
+
+
+# Bellow is 5x5 DOJO
+# close offload for gpt
+python benchmark.py --suite gpt.wsc_perf --num-hosts 5 --num-devices-per-host 5 --hardware dojo --rst_folder tmp_dojo
+# open offload for gpt
+python benchmark.py --suite gpt.wsc_perf --offload --num-hosts 5 --num-devices-per-host 5 --hardware dojo --rst_folder tmp_dojo 
+# close offload for bert
+python benchmark.py --suite bert.wsc_perf --num-hosts 5 --num-devices-per-host 5 --hardware dojo --rst_folder tmp_dojo
+# open offload for bert
+python benchmark.py --suite bert.wsc_perf --offload --num-hosts 5 --num-devices-per-host 5 --hardware dojo --rst_folder tmp_dojo 
+# close offload for wresnet
+python benchmark.py --suite wresnet.wsc_perf --num-hosts 5 --num-devices-per-host 5 --hardware dojo --rst_folder tmp_dojo
+# open offload for wresnet
+python benchmark.py --suite wresnet.wsc_perf --offload --num-hosts 5 --num-devices-per-host 5 --hardware dojo --rst_folder tmp_dojo 
